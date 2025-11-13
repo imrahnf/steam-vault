35622c6d7b600c6c4f1c7c958b3408a8adb74854
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-11-13 14:47:45 -0500

    db

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -15,14 +15,7 @@ HOST = os.getenv("host")
 PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
 
-# Fetch database variables
-if all([USER, PASSWORD, HOST, PORT, DBNAME]):
-    print("Using Supabase Postgres")
-    # Construct the SQLAlchemy connection string
-    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
-else:
-    print("Using local SQLite database")
-    DATABASE_URL = "sqlite:///./steamvault.db"
+DATABASE_URL = "sqlite:///./steamvault.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
