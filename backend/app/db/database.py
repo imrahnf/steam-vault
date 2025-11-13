3670325591996cae4dafcd1ae2f2d2b87ccb1c32
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-11-13 01:39:18 -0500

    connection string changes

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -8,19 +8,21 @@ from .models import Base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
 # Fetch database variables
-if not DATABASE_URL:
-    print("Using Supabase URL")
-    USER = os.getenv("user")
-    PASSWORD = os.getenv("password")
-    HOST = os.getenv("host")
-    PORT = os.getenv("port")
-    DBNAME = os.getenv("dbname")
+if all([USER, PASSWORD, HOST, PORT, DBNAME]):
+    print("Using Supabase Postgres")
+    # Construct the SQLAlchemy connection string
     DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 else:
-    print("Using SQLite database")
+    print("Using local SQLite database")
+    DATABASE_URL = "sqlite:///./steamvault.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
